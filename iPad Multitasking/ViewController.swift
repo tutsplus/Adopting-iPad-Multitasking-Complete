a0d137b5ce00c712580e56e8613db941f76b2ee5
Davis Allie <davis525@live.com.au>
2015-11-01 18:27:00 +1100

    Completed Project

--- iPad Multitasking/ViewController.swift
+++ iPad Multitasking/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import AVFoundation
 import AVKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVPlayerViewControllerDelegate {
 
     @IBOutlet weak var viewWidth: UILabel!
     @IBOutlet weak var sizeClass: UILabel!
@@ -28,9 +28,47 @@ class ViewController: UIViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let destination = segue.destinationViewController as!
         AVPlayerViewController
+        destination.delegate = self
         let url = NSBundle.mainBundle().URLForResource("Video", withExtension: "mp4")!
         destination.player = AVPlayer(URL: url)
         destination.player?.play()
     }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+        
+        self.viewWidth.text = "\(size.width)pts wide"
+    }
+
+    override func willTransitionToTraitCollection(newCollection: UITraitCollection, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        super.willTransitionToTraitCollection(newCollection, withTransitionCoordinator: coordinator)
+        
+        switch newCollection.horizontalSizeClass {
+        case .Compact:
+            self.sizeClass.text = "Compact Horizontal Size Class"
+        case .Regular:
+            self.sizeClass.text = "Regular Horizontal Size Class"
+        case .Unspecified:
+            self.sizeClass.text = "Unspecified Horizontal Size Class"
+        }
+    }
+    
+    func playerViewControllerShouldAutomaticallyDismissAtPictureInPictureStart(playerViewController: AVPlayerViewController) -> Bool {
+        return false
+    }
+    
+    // MARK: AVPictureInPictureController Example
+    /*self.pictureinPictureController = AVPictureInPictureController(playerLayer: layer)
+    self.pictureinPictureController.delegate = // Any object conforming to the AVPictureInPictureViewControllerDelegate protocol
+    if AVPictureInPictureController.isPictureInPictureSupported() {
+    // Display button to enter Picture in Picture
+    }
+    
+    ...
+    
+    
+    func pictureinPictureButtonPressed() {
+        self.pictureInPictureController.startPictureInPicture()
+    }*/
 }
 
